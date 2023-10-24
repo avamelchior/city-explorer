2c7c0de81b7abacd15f09343cba26d2c0ed7a901
tektechnologies <info@tektechnologies.com>
2023-10-24 09:57:45 -0500

    add locIq url

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+// import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_KEY = import.meta.env.VITE_API_KEY;
+// console.log(API_KEY);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false,
+      cityName: "",
+    };
+  }
+
+  handleCityInput = (event) => {
+    // console.log('life!');
+    this.setState({
+      cityName: event.target.value,
+    });
+  };
+
+  handleCityFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log("city name?", this.state.cityName);
+    console.log(API_KEY);
+
+    let URL = `http://us1.locationiq.com/v1/search ? key=${API_KEY}  & q=${this.state.cityName}    &format=json`;
+
+    
+  //   let cityInfo = await axios.get(URL);
+  //   console.log(cityInfo);
+  //   console.log("city info: ", cityInfo.data[0]);
+
+  //   // this.setState({})
+  };
+
+  render() {
+    // console.log(this.state.cityName);
+    return (
+      <>
+        <h1>Welcome to World Explore!</h1>
+        <form onSubmit={this.handleCityFormSubmit}>
+          <label>
+            Pick a City:
+            <input type="text" onChange={this.handleCityInput} />
+          </label>
+          <button type="submit"> Get City Data</button>
+        </form>
+
+        {/* {this.state.error ? (
+          <p>this.state.errorMessage</p>
+        ) : (
+          <ul>{cityData}</ul>
+        )} */}
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
